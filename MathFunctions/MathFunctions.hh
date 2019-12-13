f5f08cc257547aca0c50bad83a896279bb590f60
Tetramad <cbwjd777@naver.com>
2019-12-13 20:29:19 +0900

    Adding System Introspection

--- MathFunctions/MathFunctions.hh
+++ MathFunctions/MathFunctions.hh
@@ -1,4 +1,3 @@
 #pragma once
-#include <cmath>
 
 auto mysqrt(double) -> double;
--- MathFunctions/mysqrt.cc
+++ MathFunctions/mysqrt.cc
@@ -1,6 +1,18 @@
 #include "MathFunctions.hh"
+#if defined(HAVE_CXX_STD_LOG) && defined(HAVE_CXX_STD_EXP)
+#include <cmath>
+#include <iostream>
+#endif
 
 auto mysqrt(double x) -> double
 {
-    return std::sqrt(x);
+#if defined(HAVE_CXX_STD_LOG) && defined(HAVE_CXX_STD_EXP)
+    double result = std::exp(std::log(x) * 0.5);
+    std::cout << "Computing sqrt of " << x << " to be " << result
+                << " using std::log and std::exp in cmath" << std::endl;
+#else
+    double result = x;
+#endif
+
+    return result;
 }
